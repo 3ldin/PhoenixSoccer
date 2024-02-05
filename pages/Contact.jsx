0631f1be28e6320c13dbe2980e68897311682426
3ldin <raceman440@gmail.com>
2024-02-05 16:55:24 -0500

    fix

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import Navbar from '../components/Navbar';
+import Navbar from '@/components/navbar';
 import Footer from '../components/Footer';
 import emailjs from '@emailjs/browser';
 
